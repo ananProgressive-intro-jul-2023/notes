c4dc53c721a9ca258503d4b2f4e7a138ebc3d514
ananProgressive <Adam_Nan@progressive.com>
2023-07-19 11:25:25 -0400

    Day 3

---
+++ FirstAppSolution/FirstApp/Models/StatusResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace FirstApp.Models;
+
+    public record StatusResponseModel(DateTime LastChecked, string Message, string Status);
+
+public record TodoItemModel(Guid Id, string Description, string Status);
+
+ 
